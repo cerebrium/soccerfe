6a44a5b88e222731259bfbedccf781d6ca41f3e5
Nicholas Shankland <nicholas.m.shankland@gmail.com>
2022-07-30 19:03:16 -0700

    initial layout setup

---
+++ src/shared/nav/nav.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./styles/style.css";
+
+const Nav: React.FC = (): JSX.Element => (
+  <nav className="nav">
+    <section className="logo-container">
+      <Link to="/" className="logo-link">
+        {" "}
+        Logo
+      </Link>
+    </section>
+    <title className="title">Soccer</title>
+    <section className="profile-link">
+      <Link to="/profile">Profile</Link>
+    </section>
+  </nav>
+);
+
+export default Nav;
